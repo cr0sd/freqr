3e3b7a17a8fa1b0da505058981173cf6ba66702d
pvtroswold <pvtroswold@users.noreply.github.com>
2019-03-23 10:18:21 -0500

    Update freqr.c

--- freqr.c
+++ freqr.c
@@ -15,6 +15,16 @@ char szFn[4][3]={"sn","sq","sw","tr"};
 FILE *fer;
 
 /********
+ * sn(f,o,r,a)
+ * Generate sine wave
+ * sn(x,f) = sin( (2*pi)/period * x ) * amplitude
+ * 
+ ********/
+int32_t sn(double f,double o,double r,double a)
+{
+	return sin(((2.0L*pi)/(r/f))*o)*(a/2.0L);
+}
+/********
  * sq(f,o,r,a)
  * Generate single 50% duty cycle square wave 
  * at frequency f, offset o, samplerate r, and
@@ -23,28 +33,19 @@ FILE *fer;
  ********/
 int32_t sq(double f,int32_t o,int32_t r,int32_t a)
 {
-	return o%(int32_t)(r/f)<(int32_t)(r/f)/2?a:-a;
+	return o%(int32_t)(r/f)<(int32_t)(r/f)/2?(a/2.0L):-(a/2.0L);
 }
 
 int32_t tr(double f,int32_t o,int32_t r,int32_t a)
 {
 	// return 2*a/3.1415926f*asin(sin(2*3.1415926f/((r/f)*o)));
-	return ((a*((r/f)-(int32_t)fabs(o%(int32_t)(2*(r/f))-(r/f)) ))/(r/f))-(a/2);
+	// return ((a*((r/f)-(int32_t)fabs(o%(int32_t)(2*(r/f))-(r/f)) ))/(r/f))-(a/2);
+	return ((a*((r/f/2.0L)-(int32_t)fabs((o+(int32_t)(r/f/4.0L))%(int32_t)(2*(r/f/2.0L))-(r/f/2.0L)) ))/(r/f/2.0L))-(a/2.0L);
 }
 
 int32_t sw(double f,int32_t o,int32_t r,int32_t a)
 {
-	return fmod((double)o,((double)r/f))/((double)r/f)*(double)a;
-}
-/********
- * sn(f,o,r,a)
- * Generate sine wave
- * sn(x,f) = sin( (2*pi)/period * x ) * amplitude
- * 
- ********/
-int32_t sn(double f,double o,double r,double a)
-{
-	return sin(((2.0L*pi)/(r/f))*o)*(double)a;
+	return fmod((double)(o+(int32_t)(r/f/2.0L)),((double)r/f))/((double)r/f)*(double)a-(a/2.0L);
 }
 
 int main(int argc,char **argv)
@@ -56,13 +57,12 @@ int main(int argc,char **argv)
 	//16 bit / 2 bytes per sample @ 44.1k samples per sec = 705.6kB
 	
 	FILE *fo=fopen("raw.dat","wb");
-	if(!fo)return 4;
 	
 	uint32_t samplerate=44100;
 	uint32_t samples=samplerate;
 	uint8_t bitdepth=16;
 	double f=440;
-	uint32_t a=900;
+	int32_t a=900;
 	uint8_t fn=0;
 	
 	fer=fopen("log.txt","w");
@@ -95,6 +95,9 @@ int main(int argc,char **argv)
 		return 1;
 	}
 	
+	
+	fprintf(fer,"fn:%s\n",szFn[fn]);
+	
 	int16_t *b=malloc(sizeof(int16_t)*samples);
 	if(!b)printf("fail\n");
 	
@@ -116,9 +119,12 @@ int main(int argc,char **argv)
 			b[i]=tr(f,i,samplerate,a);
 		}
 		fprintf(fer,"b:%i\n",b[i]);
+		
+		// b[i]+=sq(raisesemi(f,7),i,samplerate,700.0);
+		// b[i]/=2;
 	}
 	
-	fwrite(b,sizeof(int16_t),samples,fo); //sizeof(uint16_t)==2 (bytes)!
+	fwrite(b,sizeof(int16_t),samples,fo);
 	
 	free(b);
 	fclose(fer);
