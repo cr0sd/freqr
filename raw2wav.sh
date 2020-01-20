b8783718793583a0bc312d72ee0ccf48249874e4
cr0sd <jumpingkanga@outlook.com>
2020-01-20 08:21:14 -0600

    raw2wav.sh: Added I/O options

--- raw2wav.sh
+++ raw2wav.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
+_this=raw2wav.sh
+
+# Get input file
+if [ $# = 0 ]; then
+	echo "$_this: Usage: $_this [inputfile [outputfile]]"
+	_inputfile="raw.dat"
+else
+	_inputfile=$1
+	if [ -f $_inputfile ]; then
+		:
+	else
+		echo "$_this: Error: Could not find input file \"$_inputfile\". Exiting"
+		exit
+	fi
+fi
+
+# Get output file
+if [ $# -lt 2 ]; then
+	_outputfile="raw.wav"
+else
+	_outputfile=$2
+	if [ -f $_outputfile ]; then
+		echo "$_this: Warning: \"$_outputfile\" exists. Overwriting."
+	fi
+fi
+
 # Create 'raw.wav' from 'raw.dat'
-sox -b 16 -c 1 -r 44100 -e signed-integer -t raw raw.dat raw.wav
+sox -b 16 -c 1 -r 44100 -e signed-integer -t raw $_inputfile $_outputfile
